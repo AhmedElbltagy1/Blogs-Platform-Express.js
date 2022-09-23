5a7aad09fa1ab0f2a84d99143539a4535628a87e
AhmedElbltagy1 <ahmedelbltagy1999@gmail.com>
2022-09-23 15:42:05 +0200

    adding errors expressions

---
+++ utils/errors.js
@@ -0,0 +1,8 @@
+module.exports = {
+    TOKEN_NOT_AUTHENTICATED: "You're not authenticated", // THIS WILL BE USED ONLY IN IS-AUTH MIDDLEWARE.
+    NOT_AUTHENTICATED: "You're not authenticated.",
+    NOT_AUTHORIZED: "You're not authorized.",
+    INVALID_TOKEN: "Token is invalid.",
+    ALREADY_EXIST: "Resource already exists",
+    NOT_FOUND: "Resource is not found"
+}
