084a67364eb8a1f388f9242454d7d9f2a04c2baa
AhmedElbltagy1 <ahmedelbltagy1999@gmail.com>
2022-10-12 23:54:50 +0200

    some changes

--- Services/users/user.route.js
+++ Services/users/user.route.js
@@ -1,19 +1,12 @@
 const router = require("express").Router();
-
 const userController= require("./user.controller");
-
 const is_Auth = require("../../Middlewares/is-Auth");
-
 const validateSchema = require("../../Middlewares/validateSchema");
-
 const {loginSchema,RegisterSchema } = require("./user.validation");
-
 const { GET_USERS,GET_USER,UPDATE_USER,DELETE_USER } = require("./endPoint");
 
 
-
 router.post("/signin",validateSchema(loginSchema),userController.signin)
-
 router.post("/signup",validateSchema(RegisterSchema),userController.signup)
  
 // router.get("/:id",is_Auth(GET_USER),userController.getUser);
@@ -23,6 +16,5 @@ router.post("/signup",validateSchema(RegisterSchema),userController.signup)
 // router.put("/:id",is_Auth(UPDATE_USER),userController.updateUser)
 
 // router.delete("/:id",is_Auth(DELETE_USER),userController.deleteUser)
-
-
 module.exports = router;
+
